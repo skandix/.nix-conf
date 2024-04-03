0ab150e8a36d680bb5b266b6dbe3e83c3c3d1cd9
Bendik Dyrli <skandix@protonmail.com>
2024-04-03 14:22:29 +0200

    fuck this fucking shit fuck fuck fuck FUCK
    Signed-off-by: Bendik Dyrli <skandix@protonmail.com>

--- common/networkmanager.nix
+++ common/networkmanager.nix
@@ -17,15 +17,6 @@
 
   systemd.services.NetworkManager-wait-online.enable = false;
   systemd.network.wait-online.enable = false;
-  boot.initrd.systemd.network.wait-online.enable = false;
-
-  # systemd.network.wait-online.timeout = 0;
-
-  # systemd.network.wait-online.ignoredInterfaces = [
-  #   "docker0"
-  #   "tailscale0"
-  #   "lo"
-  # ];
 
   services.tailscale = {
     enable = true;
